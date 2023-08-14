86edc6bfa8e99fb2780df51fd7dc4ea734a7afa2
kushtarbek <dr.kushtarbek@gmail.com>
2023-08-14 17:17:37 -0500

    added CLI game not broken into MVC

---
+++ src/controller/Main.java
@@ -0,0 +1,8 @@
+package controller;
+
+public class Main {
+    public static void main(String[] args) {
+    MemoryGame2 memoryGame = new MemoryGame2();
+        System.out.println(memoryGame);
+    }
+}
